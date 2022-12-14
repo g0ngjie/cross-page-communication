69989d2365f35839da2c731f4a9a221da438a056
Gj <gongjie0422@163.com>
2022-12-14 11:30:14 +0800

    fix: modify home

--- packages/examples/src/pages/home.vue
+++ packages/examples/src/pages/home.vue
@@ -1,3 +1,19 @@
 <template>
-    home
+  <div class="container">
+    <router-link to="storage">storage</router-link>
+    <router-link to="channel">channel</router-link>
+    <router-link to="worker">worker</router-link>
+    <router-link to="opener">opener</router-link>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #666;
+}
+</style>
